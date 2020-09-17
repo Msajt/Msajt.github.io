27cf3bfe711374f772f6364a21057b15edbacfe6
Msajt <paulocesarmedeiross@gmail.com>
2020-09-17 09:47:22 -0300

    Site IOT

---
+++ server_iot/lampada.js
@@ -0,0 +1,66 @@
+
+function liga1(){
+    var img = document.getElementById("luz1");
+    img.src = "Lon.png";
+}
+function desliga1(){
+    var img = document.getElementById("luz1");
+    img.src = "Loff.png";
+}
+
+//
+
+function liga2(){
+    var img = document.getElementById("luz2");
+    img.src = "Lon.png";
+}
+function desliga2(){
+    var img = document.getElementById("luz2");
+    img.src = "Loff.png";
+}
+
+//
+
+function liga3(){
+    var img = document.getElementById("luz3");
+    img.src = "Lon.png";
+}
+function desliga3(){
+    var img = document.getElementById("luz3");
+    img.src = "Loff.png";
+}
+
+//
+
+function liga4(){
+    var img = document.getElementById("luz4");
+    img.src = "Lon.png";
+}
+function desliga4(){
+    var img = document.getElementById("luz4");
+    img.src = "Loff.png";
+}
+
+//
+
+function liga5(){
+    var img = document.getElementById("luz5");
+    img.src = "Lon.png";
+}
+function desliga5(){
+    var img = document.getElementById("luz5");
+    img.src = "Loff.png";
+}
+
+//
+
+function liga6(){
+    var img = document.getElementById("luz6");
+    img.src = "Lon.png";
+}
+function desliga6(){
+    var img = document.getElementById("luz6");
+    img.src = "Loff.png";
+}
+
+
---
+++ server_iot/lista1.js
@@ -0,0 +1,4 @@
+function mudar(){
+    for(var i=0; i<10; i++) alert("Mudou " + (i+1));
+    document.getElementById("result").innerHTML = "mudou";
+}
