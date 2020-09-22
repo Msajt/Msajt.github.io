23eb66a2cb0669576612d9baa27b32b061ccaac6
Msajt <paulocesarmedeiross@gmail.com>
2020-09-22 14:29:13 -0300

    server iot atualizado

--- server_iot/lampada.js
+++ server_iot/lampada.js
@@ -1,66 +1,14 @@
 
-function liga1(){
-    var img = document.getElementById("luz1");
-    img.src = "Lon.png";
-}
-function desliga1(){
-    var img = document.getElementById("luz1");
-    img.src = "Loff.png";
-}
+function liga(luz){
+    var img = document.getElementById(luz);
+    var img1 = "Loff.png", img2 = "Lon.png";
 
-//
+   // alert(img.src);
 
-function liga2(){
-    var img = document.getElementById("luz2");
-    img.src = "Lon.png";
-}
-function desliga2(){
-    var img = document.getElementById("luz2");
-    img.src = "Loff.png";
-}
+    img.src = (img.src.match(img1)) ? img2 : img1;
 
-//
+    //alert(img.src);
 
-function liga3(){
-    var img = document.getElementById("luz3");
-    img.src = "Lon.png";
-}
-function desliga3(){
-    var img = document.getElementById("luz3");
-    img.src = "Loff.png";
-}
-
-//
-
-function liga4(){
-    var img = document.getElementById("luz4");
-    img.src = "Lon.png";
-}
-function desliga4(){
-    var img = document.getElementById("luz4");
-    img.src = "Loff.png";
-}
-
-//
-
-function liga5(){
-    var img = document.getElementById("luz5");
-    img.src = "Lon.png";
-}
-function desliga5(){
-    var img = document.getElementById("luz5");
-    img.src = "Loff.png";
-}
-
-//
-
-function liga6(){
-    var img = document.getElementById("luz6");
-    img.src = "Lon.png";
-}
-function desliga6(){
-    var img = document.getElementById("luz6");
-    img.src = "Loff.png";
 }
 
 
