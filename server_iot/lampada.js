85057f90fbb1e83631e5310b1444df5d05040778
Msajt <paulocesarmedeiross@gmail.com>
2020-10-10 13:57:41 -0300

    Atualização - Lista 2

--- server_iot/lampada.js
+++ server_iot/lampada.js
@@ -2,13 +2,26 @@
 function liga(luz){
     var img = document.getElementById(luz);
     var img1 = "Loff.png", img2 = "Lon.png";
-
-   // alert(img.src);
+    var sinal;
 
     img.src = (img.src.match(img1)) ? img2 : img1;
+    sinal = GetRandomNumbers(0,1000);
+    SendToAPI(sinal);
+}
 
-    //alert(img.src);
+function SendToAPI(state){
+    var KEY = "JOZ0BRKN60MCFD8K";
+    //criar um objeto capaz de enviar dados via requisição HTTP GET
+    const http = new XMLHttpRequest();
+    //prepara um GET passando a váriavel lux como ultimo paramentro do link
+    http.open("GET", "https://api.thingspeak.com/update?api_key="+ KEY +"&field1=0"+state);
+    //envia um GET
+    http.send();
+    //quando a requisição retornar ele chama o console e imprime
+}
 
+function GetRandomNumbers(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
